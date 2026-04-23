1d8883a541abfb498f1b53be77f956c9c0efa7a8
KaelShowTile <kaelshowtile@gmail.com>
2026-04-23 10:47:10 +1000

    2026042301

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 export default {
   async fetch(request, env, ctx) {
+    const providedKey = request.headers.get('X-API-Key');
+    const EXPECTED_KEY = "w6`?k(b);KH09nDc3QO5`8<WhTs.@Mb7#~qe9k";
+
+    if (!providedKey || providedKey !== EXPECTED_KEY) {
+      return new Response(JSON.stringify({ 
+        error: 'Unauthorized', 
+        message: 'Invaild API Key!' 
+      }), { 
+        status: 401,
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }
+
     if (request.method === 'POST' && new URL(request.url).pathname === '/sync') {
       try {
         const data = await request.json();
-        console.log("Received data from C# soft:", data);
+        console.log("Received data from your soft:", data);
 
         return new Response(JSON.stringify({ 
           status: 'success', 
